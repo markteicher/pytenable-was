7cc4e02874264910158efecbfab773a548bc3f24
Mark Teicher <mark.teicher@gmail.com>
2025-12-12 11:03:23 -0500

    Update __init__.py

--- pytenable_was/__init__.py
+++ pytenable_was/__init__.py
@@ -1,1 +1,39 @@
+"""
+pytenable-was
+A modular SDK for the Tenable Web Application Scanning (WAS) v2 API.
+"""
 
+__version__ = "1.0.0"
+
+# Core HTTP client
+from .http import HTTPClient
+
+# SDK API modules
+from .scans import ScansAPI
+from .findings import FindingsAPI
+from .vulns import VulnsAPI
+from .plugins import PluginsAPI
+from .templates import TemplatesAPI
+from .user_templates import UserTemplatesAPI
+from .folders import FoldersAPI
+from .filters import FiltersAPI
+from .notes import NotesAPI
+
+# Utilities
+from .utils import flatten_dict, pretty_json
+
+__all__ = [
+    "__version__",
+    "HTTPClient",
+    "ScansAPI",
+    "FindingsAPI",
+    "VulnsAPI",
+    "PluginsAPI",
+    "TemplatesAPI",
+    "UserTemplatesAPI",
+    "FoldersAPI",
+    "FiltersAPI",
+    "NotesAPI",
+    "flatten_dict",
+    "pretty_json",
+]
